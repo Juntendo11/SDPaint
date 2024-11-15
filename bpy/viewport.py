a3961f69984dff4e179b24754231cef86ca23984
Juntendo11 <86496557+Juntendo11@users.noreply.github.com>
2024-11-15 15:47:46 +1000

    Add files via upload

--- bpy/viewport.py
+++ bpy/viewport.py
@@ -7,3 +7,28 @@ def get_viewport_size():
                 if r.type == 'WINDOW':
                     return r.width, r.height
                     #print(f"Viewport dimensions: {r.width}x{r.height}, approximate aspect rato: {round(r.width/r.height, 2)}")
+                    
+
+def ceil_divisible(n, m) :
+    q = int(n / m)  #lower ceil
+    n1 = m * q
+    return n1
+    
+    
+def crop_image(img):
+    width, height = img.size
+    
+    #Find lowest divisible factor by 8
+    width_crop = ceil_divisible(width)
+    height_crop = ceil_divisible(height)
+    
+    dw = width - width_crop
+    dh = height - height_crop
+    
+    
+    #If odd, +1 pixel to make it even, if even => unchanged
+    dw += dw % 2
+    dh += dh % 2
+        
+    cropped = im.crop((dw/2, dh/2, dw/2, dh/2))   #(left, top, right, bottom)
+    return cropped
