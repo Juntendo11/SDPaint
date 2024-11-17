f1c2a858c59ce09744e55f14a59d38804200a0bb
Juntendo11 <86496557+Juntendo11@users.noreply.github.com>
2024-11-17 22:52:52 +1000

    Add files via upload

--- img/img_process.py
+++ img/img_process.py
@@ -1,19 +1,24 @@
 from PIL import Image
 
-
-def ceil_divisible(n, m) :
+def ceil_divisible(n, m):
+    # Find closest lower divisible number
     q = int(n / m)  #lower ceil
     n1 = m * q
     return n1
-    
-    
-def crop_image(img):
+
+def div_image_size(img):
+    #Returns the expected image resolution size
     width, height = img.size
     
     #Find lowest divisible factor by 8
     width_crop = ceil_divisible(width,8)
     height_crop = ceil_divisible(height,8)
+    return width_crop, height_crop
     
+def crop_image(img):
+    #Returns the cropped image for stable diffusion
+    width, height = img.size
+    width_crop, height_crop = div_image_size(img)
     dw = width - width_crop
     dh = height - height_crop
 
@@ -25,7 +30,3 @@ def crop_image(img):
     cropped_img = img.crop((left, top, right, bottom))
     return cropped_img
 
-ren_img = Image.open("mahousyojyo_chara_01.png")
-print(ren_img.size)
-cropped = crop_image(ren_img)
-print(cropped.size)
